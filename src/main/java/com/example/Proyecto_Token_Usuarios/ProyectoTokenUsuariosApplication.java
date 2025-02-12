e5eacf6191c1ca0c5d1a607c2b4e2a694357a2d4
gdenmor <gdenmor2001@g.educaand.es>
2025-02-12 14:57:40 +0100

    Creacion proyecto

---
+++ src/main/java/com/example/Proyecto_Token_Usuarios/ProyectoTokenUsuariosApplication.java
@@ -0,0 +1,13 @@
+package com.example.Proyecto_Token_Usuarios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoTokenUsuariosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoTokenUsuariosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Proyecto_Token_Usuarios/ProyectoTokenUsuariosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Proyecto_Token_Usuarios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoTokenUsuariosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
